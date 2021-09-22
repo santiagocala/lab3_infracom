394db4d578f1c8ad97c4041ff2f67b44168887f3
santiagocala <s.cala@uniandes.edu.co>
2021-09-22 01:03:50 -0500

    A momir

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import hashlib
 
 TCP_IP = 'localhost'
 TCP_PORT = 9001
@@ -6,73 +7,33 @@ BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-with open('received_file', 'wb') as f:
+num_cliente = s.recv(BUFFER_SIZE)
+tamanio = int(s.recv(BUFFER_SIZE))
+hashsito = hashlib.sha256()
+anterior = None
+hash_final = None
+contador = 0
+hash_solicitado = None
+with open('received_file' + str(num_cliente) + ".JPG", 'wb') as f:
     print ('file opened')
     while True:
         #print('receiving data...')
         data = s.recv(BUFFER_SIZE)
-        print('data=%s', (data))
-        if not data:
+        if contador == round(tamanio/BUFFER_SIZE):
+            hash_final = s.recv(BUFFER_SIZE).decode()
+            print("Hashfinal :" + hash_final)
+            hash_solicitado = hashsito.hexdigest()
             f.close()
             print('file close()')
             break
-        # write data to a file
+        anterior = data
+        hashsito.update(data)
         f.write(data)
-    print(str(hash(f)))
+        contador += 1
+    print("Hash final: " + str(hash_final) + " y Hash calculado: " + str(hash_solicitado))
+    if hash_final == hash_solicitado:
+        print("A momir")
 
-
-# with open('received_file', 'wb') as f:
-#     print 'file opened'
-#     anterior = None
-#     hash_value = None
-#     while True:
-#         #print('receiving data...')
-#         data = s.recv(BUFFER_SIZE)
-#         print('data=%s', (data))
-#         if not data:
-#             f.close()
-#             hash_value = anterior
-#             print 'file close()'
-#             break
-#         # write data to a file
-#         anterior = data
-#         f.write(data)
-    
-
-# with open('received_file', 'wb') as f:
-#     print ('file opened')
-#     datos = ""
-#     hash_value = ""
-#     contador = 0
-#     while True:
-#         #print('receiving data...')
-#         data = s.recv(BUFFER_SIZE)
-#         if contador == 0:
-#             datos = data
-#         elif contador == 1:
-#             hash_value = data
-#         print('data=%s', (data))
-#         if not data:
-#             f.close()
-#             print ('file close()')
-#             break
-#         contador += 1
-#     print("datos: " + str(hash(datos)) + " hash value : " +  str(hash_value))
-
-# with open('received_file', 'wb') as f:
-#     while True:
-#     print ('file opened')
-#         data = s.recv(BUFFER_SIZE)
-#         f.write(data)
-#         hash_value = s.recv(BUFFER_SIZE)
-#         hash_actual = str(hash(f))
-#         print(str(hash_value) + " con " + str(hash_actual.encode()))
-#         if hash_actual.encode() == hash_value:
-#             print("Todo bien")
-#         else:
-#             print("todo mal")
-#         f.close()
-#         break
         
 print('Successfully get the file')
 s.close()
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import socket
 from threading import Thread
 from socketserver import ThreadingMixIn
+import hashlib
+import os
 
-client_capacity = 4
+client_capacity = 1
 path = ""
-active_connections = 0
+contador = 0
 
 TCP_IP = 'localhost'
 TCP_PORT = 9001
@@ -14,28 +16,35 @@ hashsito = None
 
 class ClientThread(Thread):
 
-    def __init__(self,ip,port,sock):
+    def __init__(self,ip,port,sock,num_cliente):
         Thread.__init__(self)
         self.ip = ip
         self.port = port
         self.sock = sock
+        self.num_cliente = num_cliente
         print(" New thread started for "+ip+":"+str(port))
 
     def run(self):
-        filename='archivo100.txt'
+        self.sock.send(str(self.num_cliente).encode())
+        filename='IMG_5456 copy.JPG'
+        hashsito = hashlib.sha256()
         f = open(filename,'rb')
+        self.sock.send(str(os.path.getsize(filename)).encode())
         while True:
             l = f.read(BUFFER_SIZE)
             while (l):
+                hashsito.update(l)
                 self.sock.send(l)
                 #print('Sent ',repr(l))
                 l = f.read(BUFFER_SIZE)
             if not l:
-                hashsito = str(hash(f))
                 f.close()
+                # resultado = hashsito.hexdigest()
+                # print(resultado)
+                self.sock.send(hashsito.hexdigest().encode())
+                #print(hashsito.hexdigest())
                 self.sock.close()
                 break
-        print(hashsito)
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -47,7 +56,8 @@ while True:
     print("Waiting for incoming connections...")
     (conn, (ip,port)) = tcpsock.accept()
     print('Got connection from ', (ip,port))
-    newthread = ClientThread(ip,port,conn)
+    newthread = ClientThread(ip,port,conn,contador)
+    contador += 1
     threads.append(newthread)
     print("Se agregó un nuevo thread: Nueva longitud: " + str(len(threads)))
     if len(threads) == client_capacity:
