41c444eedf0d6500322729d8678c2d563c205b16
Juan Diego Martínez Camacho <jd.martinezc@uniandes.edu.co>
2021-09-29 11:17:40 -0500

    Avance logs

--- client.py
+++ client.py
@@ -1,35 +1,76 @@
 import socket
 import hashlib
 import math
+import time
+import scapy
 from threading import Thread
+
+### Configuration ###
+CLIENTS = 10
+#####################
 
 SERVER_IP = "localhost"
 SERVER_PORT = 8080
 BUFFER_SIZE = 1024
 
-CLIENTS = 10
+log = {}
 
 class SocketThread(Thread):
 
+    def __init__(self, socket, port):
+        Thread.__init__(self)
+        self.socket = socket
+        self.port = port
+        self.hash = hash
+
     def run(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((SERVER_IP, SERVER_PORT))
-        number = int.from_bytes(s.recv(1), "big")
-        size = int.from_bytes(s.recv(4), "big")
-        hash = s.recv(32).hex()
-        with open("file" + str(number), "wb") as f:
-            hashBuffer = hashlib.sha256()
-            for b in range(size):
-                l = s.recv(BUFFER_SIZE)
+        hashBuffer = hashlib.sha256()
+        iterations = int(math.ceil(log[self.port]["fileSize"]/BUFFER_SIZE))
+        with open("Cliente" + log[self.port]["number"] + "-Prueba-" + str(CLIENTS), "wb") as f:
+            initialTime = time.time()
+            for i in range(iterations):
+                l = self.socket.recv(BUFFER_SIZE)
                 f.write(l)
                 hashBuffer.update(l)
-        hashCalculado = hashBuffer.hexdigest()
-        s.close()
-        print("Cliente", number, "-", "Hash correcto:", hash == hashCalculado)
+            self.socket.send(True) # Received last package
+            log[self.port]["time"] = int(time.time() - initialTime)
+        log[self.port]["success"] = (hash == hashBuffer.hexdigest())
+        self.socket.send(log[self.port]["success"])
 
 clients = []
 
 for c in range(CLIENTS):
-    client = SocketThread()
-    client.start()
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect((SERVER_IP, SERVER_PORT))
+    clientPort = clientSocket.getsockname()[1]
+    log[clientPort]["number"] = int.from_bytes(clientSocket.recv(1), "big")
+    log[clientPort]["fileName"] = clientSocket.recv(7).decode()
+    log[clientPort]["fileSize"] = int.from_bytes(clientSocket.recv(4), "big")
+    hash = clientSocket.recv(32).hex()
+    log[clientPort]["packets"] = 0
+    log[clientPort]["bytes"] = 0
+    client = SocketThread(clientSocket, clientPort, hash)
     clients.append(client)
+    clientSocket.send(True) # Ready to receive
+
+## Start sniff
+
+sniffer = scapy.AsyncSniffer("scr port " + str(SERVER_PORT))
+sniffer.start()
+
+for c in clients:
+    c.start()
+
+for c in clients:
+    c.join()
+
+sniffer.stop()
+
+for packet in sniffer:
+    clientPort = packet["TCP"].dport
+    log[clientPort]["packets"] = log[clientPort]["packets"] + 1
+    log[clientPort]["bytes"] = log[clientPort]["bytes"] + len(packet)
+
+# Create log
+# Recorrer clientes
+c.socket.close()
--- server.py
+++ server.py
@@ -2,51 +2,81 @@ import socket
 import hashlib
 import os
 import math
+import time
+import scapy
 from threading import Thread
+
+### Configuration ###
+CLIENTS = 10
+FILE_NAME = "100.txt"
+#####################
 
 SERVER_IP = "localhost"
 SERVER_PORT = 8080
 BUFFER_SIZE = 1024
+FILE_SIZE = os.path.getsize(FILE_NAME)
+ITERATIONS = int(math.ceil(FILE_SIZE/BUFFER_SIZE))
 
-CLIENTS = 10
-FILE = "file"
-FILE_SIZE = int(math.ceil(os.path.getsize(FILE)/BUFFER_SIZE))
+log = {}
 
 class SocketThread(Thread):
 
-    def __init__(self, ip, port, socket, number):
+    def __init__(self, socket, port):
         Thread.__init__(self)
-        self.ip = ip
-        self.port = port
         self.socket = socket
-        self.number = number
-        print("Cliente", number, "-", ip, port)
+        self.port = port
 
     def run(self):
-        self.socket.send(self.number.to_bytes(1, "big"))
-        self.socket.send(FILE_SIZE.to_bytes(4, "big"))
-        self.socket.send(hash)
-        with open(FILE, "rb") as f:
-            for b in range(FILE_SIZE):
+        with open(FILE_NAME, "rb") as f:
+            initialTime = time.time()
+            for i in range(ITERATIONS):
                 self.socket.send(f.read(BUFFER_SIZE))
-        self.socket.close()
+            self.socket.recv(1) # Received last package
+            log[self.port]["time"] = int(time.time() - initialTime)
+        log[self.port]["success"] = bool.from_bytes(self.socket.recv(1), "big") 
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 tcp.bind((SERVER_IP, SERVER_PORT))
 clients = []
 
-with open(FILE, "rb") as f:
+with open(FILE_NAME, "rb") as f:
     hashBuffer = hashlib.sha256()
-    for b in range(FILE_SIZE):
+    for i in range(ITERATIONS):
         hashBuffer.update(f.read(BUFFER_SIZE))
 hash = hashBuffer.digest()
 
 for c in range(CLIENTS):
     tcp.listen(0)
     (clientSocket, (clientIp, clientPort)) = tcp.accept()
-    client = SocketThread(clientIp, clientPort, clientSocket, c + 1)
+    log[clientPort] = {}
+    log[clientPort]["number"] = c + 1
+    log[clientPort]["packets"] = 0
+    log[clientPort]["bytes"] = 0
+    clientSocket.send(log[clientPort]["number"].to_bytes(1, "big"))
+    clientSocket.send(FILE_NAME.encode())
+    clientSocket.send(FILE_SIZE.to_bytes(4, "big"))
+    clientSocket.send(hash)
+    client = SocketThread(clientSocket, clientPort)
     clients.append(client)
+    clientSocket.recv(1) # Ready to receive
+
+sniffer = scapy.AsyncSniffer("scr port " + str(SERVER_PORT))
+sniffer.start()
 
 for c in clients:
     c.start()
+
+sniffer.stop
+
+for c in clients:
+    c.join()
+
+for packet in sniffer:
+    clientPort = packet["TCP"].dport
+    log[clientPort]["packets"] = log[clientPort]["packets"] + 1
+    log[clientPort]["bytes"] = log[clientPort]["bytes"] + len(packet)
+
+# Create log
+#Recorrer clientes
+c.socket.close()
